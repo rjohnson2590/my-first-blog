c72aa345432fc9472e7ef66785d0fc0a95acfa50
My Name <me@example.com>
2015-01-21 13:50:54 -0800

    morning work on express jade

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'My Blog!' });
+  res.render('index', { title: 'My Blog Is A Very Fine Blog!' });
 });
 
 router.get('/pay', function(req, res) {
   res.render('pay', {title: "Where's my money, honey?"});
 });
+
+router.get('/post1', function(req, res) {
+  res.render('post1', {title: "funny"});
+});
+
+router.get('/post2', function(req, res) {
+  res.render('post2', {title: "happy"});
+});
+
+router.get('/post3', function(req, res) {
+  res.render('post3', {title: "considerate"});
+});
+
+
 
 module.exports = router;
