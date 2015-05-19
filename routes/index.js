28f59d51aa5227db53ca0a2d41db5ef6337c7895
Richard Johnson <rjohnson2590@gmail.com>
2015-05-19 16:39:55 -0700

    Routing things with js

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res) {
-  res.render('index', { title: 'The Very Fine Blog Blog' });
-});
 
-router.get('/pay', function(req, res) {
-  res.render('pay', {title: "Where's my money, honey?"});
-});
+
 
 //////////////////////////////////////
 // router.get('/post1', function(req, res) {
@@ -24,22 +19,53 @@ router.get('/pay', function(req, res) {
 // });
 //////////////////////////////////////
 
-var posts = require('./posts');
 
-router.param('post_name', function(request, response, next, post_name){
-    //note: we'll define posts.find in a minute
-    posts.find(post_name, function(post) {
-        if(post) {
-            request.post = post;
-            next();
-        } else {
-            return next(new Error('no such post as ' + post_name + '!'))
-        }
-    });
-});
 
-router.get('/posts/:post_name', function(request, response) {
-    response.render('post', {post: request.post});
-});
+// router.param('post_name', function(request, response, next, post_name){
+//     //note: we'll define posts.find in a minute
+//     posts.find(post_name, function(post) {
+//         if(post) {
+//             request.post = post;
+//             next();
+//         } else {
+//             return next(new Error('no such post as ' + post_name + '!'))
+//         }
+//     });
+// });
+
+// router.get('/posts/:post_name', function(request, response) {
+//     response.render('post', {post: request.post});
+// });
+
+var posts = require('../views/post');
 
 module.exports = router;
+
+// Get Home Page //
+
+    router.get('/:', function(request, response) {
+        response.render('post' + request.param('post_number'), posts[request.param('post_number')])
+    })
+
+    module.exports = router;
+
+
+router.get('/:post_name', function(request, response) {
+  // response.render('post_' + request.param('post_number'), { title: 'My Blog!' });
+  var post = posts[request.param('post_name')]
+  if (post !== undefined) {
+    response.render('post', post);
+  } else {
+    response.send(404, 'NOT FOUND')
+  }
+})
+
+router.get('/', function(req, res) {
+  res.render('index', { title: 'The Very Fine Blog Blog' });
+});
+
+router.get('/pay', function(req, res) {
+  res.render('pay', {title: "Where's my money, honey?"});
+});
+
+
